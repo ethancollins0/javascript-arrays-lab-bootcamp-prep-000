5f84f905f85362096e3e4fb022b95a961a631728
Ethan Collins <ethantcollins98@gmail.com>
2019-04-15 20:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyPrependKitten(name) {
   return kittens
 }
 
-function destructivelyRemoveLatKitten() {
+function destructivelyRemoveLastKitten() {
   kittens.pop()
   return kittens
 }
