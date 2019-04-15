62228b7c06868eaeb1ba100d5ea6c7df735d25be
Ethan Collins <ethantcollins98@gmail.com>
2019-04-15 20:07:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  return kittens.push(name)
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  
 }
